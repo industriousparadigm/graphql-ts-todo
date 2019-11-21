ccfcb6ed42ff48cab5c1fbb199766828937e9417
industriousparadigm <dsgmcosta@gmail.com>
2019-11-21 09:46:32 +0000

    proj created

---
+++ src/test/user-test-spec.ts
@@ -0,0 +1,14 @@
+import chai = require('chai')
+import chaiAsPromised = require('chai-as-promised')
+import { suite, test } from 'mocha-typescript'
+import testFunction from '../app'
+chai.use(chaiAsPromised)
+
+@suite('User Test class')
+class UserTests {
+  @test('testFunction Test - It works fine')
+  public testFunction(done) {
+    chai.expect(testFunction()).to.deep.equal('Messi and Ronaldo are legends')
+    done()
+  }
+}
